1edab6272d93c4a65c489242ed92c94a09aa4644
Mide Ojikutu <olamide.ojikutu@bskyb.com>
2015-08-27 17:22:46 +0100

    cross-browser and angular page object pattern

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -1,17 +1,32 @@
-describe('angularjs homepage todo list', function () {
-    it('should add a todo', function () {
-        browser.get('https://angularjs.org');
-        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-        element(by.css('[value="add"]')).click();
+'use strict';
 
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.get(2).getText()).toEqual('write first protractor test');
+var AngularPage = require('./pageObject/angular_page.js');
+global.angularPage = new AngularPage();
+var pageObject;
 
-        // You wrote your first test, cross it off the list
-        todoList.get(2).element(by.css('input')).click();
-        var completedAmount = element.all(by.css('.done-true'));
-        expect(completedAmount.count()).toEqual(2);
 
+describe('angularJs homepage ', function () {
+    describe('Greetings section', function () {
+        it('should greet the user', function () {
+            pageObject = angularPage.getAngularOPage();
+            pageObject.typeName('Bob');
+            expect(pageObject.greeting).toEqual('Hello Bob!')
+        });
+    });
+
+    describe('Todo list section', function () {
+        it('should add a todo', function () {
+            pageObject = angularPage.getAngularOPage();
+            pageObject.addTodo('write a protractor test');
+            expect(pageObject.todoList.count()).toEqual(3);
+        });
+    });
+
+    describe('Todo list section', function () {
+        it('should list todos', function () {
+            pageObject = angularPage.getAngularOPage();
+            expect(pageObject.todoList.count()).toEqual(2);
+            expect(pageObject.todoAt(1)).toEqual('build an angular app');
+        });
     });
 });
